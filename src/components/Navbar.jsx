a12b0e5bc2a6442d4d1724b1131ee597f4b54387
jou.junior <jou.junior@salvador.ba.gov.br>
2025-08-07 14:08:21 -0300

    Inclusão do navbar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,20 @@
+import { Link } from "react-router-dom";
+const Navbar = () => {
+  return (
+    <nav className="navbar">
+      <Link to={`/`}>Blog</Link>
+      <ul>
+        <li>
+          <Link to={`/`}>Home</Link>
+        </li>
+        <li>
+          <Link to={`/new`} className="new-btn">
+            Novo Post
+          </Link>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
